163607529c1e1f487ce8a70994adeef60b8705eb
Alex Norton <alexnorton@fastmail.com>
2017-02-09 22:27:44 +0000

    update graph to use new format and add tweet markers

--- site/src/index.js
+++ site/src/index.js
@@ -8,21 +8,26 @@ require('./index.css');
 const renderChart = (data) => {
   MG.data_graphic({
     title: 'Followers',
-    data: data.stats.map(s => ({
-      date: new Date(s[0]),
-      followers: s[1],
+    data: data.followers.map(d => ({
+      date: new Date(d.date),
+      count: d.count,
     })),
     width: 1100,
     height: 650,
+    left: 60,
     target: '#chart',
     x_accessor: 'date',
-    y_accessor: 'followers',
-    min_y_from_data: true,
+    y_accessor: 'count',
+    min_y: 15600,
     inflator: 1000 / 999,
     yax_format: d3.format(','),
+    markers: data.tweets.map(t => ({
+      date: new Date(t.date),
+      label: '',
+    })),
   });
 };
 
-fetch('https://s3-eu-west-1.amazonaws.com/twitter-stats/stats.json')
+fetch('https://s3-eu-west-1.amazonaws.com/twitter-stats/stats-new.json')
   .then(res => res.json())
   .then(data => renderChart(data));
